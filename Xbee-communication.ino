eae7c6ada033b34ba30fdfbf08c0e25160cd1523
ReebaR <78039559+RebS193@users.noreply.github.com>
2021-07-25 18:26:27 +0100

    Create Xbee-communication.ino

---
+++ Xbee-communication.ino
@@ -0,0 +1,29 @@
+//This code sets up two way Xbee communication
+//This code is used to check how the two Xbee modules are communicating with each other with the help of serial monitor
+
+#include <SoftwareSerial.h>
+SoftwareSerial XBee(2, 3); // RX, TX
+
+void setup()
+{
+  // Set up both ports at 9600 baud. This value is most important
+  // for the XBee. Make sure the baud rate matches the config
+  // setting of your XBee.
+  XBee.begin(9600);
+  Serial.begin(9600);
+  XBee.println("Start");
+   
+}
+
+void loop()
+{
+ 
+  if (Serial.available())
+  { // If data comes in from serial monitor, send it out to XBee
+    XBee.write(Serial.read());
+  }
+  if (XBee.available())
+  { // If data comes in from XBee, send it out to serial monitor
+    Serial.write(XBee.read());
+  }
+}
